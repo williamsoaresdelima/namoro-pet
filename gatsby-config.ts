6711c318d6dbd1edb91eb1655a7c3df534282930
william soares <ext.william.lima@burgerking.com.br>
2022-10-28 17:19:59 -0300

    git-action CDN

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,13 +2,15 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://williamsoaresdelima.github.io/namoro-pet`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {},
+    },
+  ],
 }
 
 export default config
